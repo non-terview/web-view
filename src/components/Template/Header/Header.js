6e916004d423efab0973ce66d6f2e53cfa77026a
Kim Hyunjae <loma1016@gmail.com>
2021-09-13 23:53:02 +0900

    Header 종료 #7

--- src/components/Template/Header/Header.js
+++ src/components/Template/Header/Header.js
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 const HeaderWrap = styled.div`
   width: 100%;
   height: 50px;
-  background-color: red;
+  background-color: pink;
   display: flex;
   align-items: center;
   padding: 0 20px;
