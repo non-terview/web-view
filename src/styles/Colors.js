5b5d1ed90ee1e87da023e0ba4c4bf8350d69684a
Kim Hyunjae <loma1016@gmail.com>
2021-09-19 23:18:08 +0900

    Color 수정 #32
    - primaryColor
    - highlightingColor
    
    추가

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -1,13 +1,8 @@
 const Colors = {
-  primaryColor: '#ff4b2b',
-  colorBlack: '#000',
-  colorWhite: '#fff',
-  colorGrey: '#b9c1ca',
-  colorSideBar: '#F8F8F9',
-  primaryGradientColor: 'linear-gradient(to right, #ff4b2b, #ff416c)',
-  colorGradient: '#ff416c',
-  colorButtonHover: '#E6EBEF',
-  colorPrice: '#0D7B99'
+  primaryColor: '#267FF7',
+  highlightingColor: '#3367FF',
+  colorWhite: '#ffffff',
+  colorBlack: '#000000'
 };
 
 export default Colors;
