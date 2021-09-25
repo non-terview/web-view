cd9a6b6e0cc7f804a83cdc300975638ac7dbef41
wlsah0814 <71693517+wlsah0814@users.noreply.github.com>
2021-09-25 19:03:25 +0900

    게시판 글 목록 페이지 (#47)
    게시판 글 목록 페이지 디자인 수정

--- src/components/BoardList/BoardList.js
+++ src/components/BoardList/BoardList.js
@@ -1,28 +1,95 @@
 import React from 'react';
-import {Component} from 'react';
+import { Component } from 'react';
+import styled from 'styled-components';
+
+const BoardNumber = styled.div`
+  background-color: #ffffff;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoardRegion = styled.div`
+  background-color: #ffffff;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoardTitle = styled.div`
+  background-color: #ffffff;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoardWriteData = styled.div`
+  background-color: #ffffff;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoardClosingDate = styled.div`
+  background-color: #ffffff;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 class BoardList extends Component {
-    render() {
-        return (
-            <div>
-                <table border='1'>
-                    <th>A</th>
-                    <th>B</th>
-                    <th>C</th>
-                    <th>D</th>
-                    <th>E</th>
-
-                    <tr>
-                        <td>test1</td>
-                        <td>test2</td>
-                        <td>test3</td>
-                        <td>test4</td>
-                        <td>test5</td>
-                    </tr>
-                </table>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <table border='1'>
+          <tr>
+            <th width='60px'>
+              <BoardNumber>글 번호</BoardNumber>
+            </th>
+            <th width='150px'>
+              <BoardRegion>지역</BoardRegion>
+            </th>
+            <th width='400px'>
+              <BoardTitle>기업명/제목</BoardTitle>
+            </th>
+            <th width='100px'>
+              <BoardWriteData>작성날짜</BoardWriteData>
+            </th>
+            <th width='100px'>
+              <BoardClosingDate>마감일</BoardClosingDate>
+            </th>
+          </tr>
+          <tr>
+            <td width='60px'>
+              <BoardNumber>1</BoardNumber>
+            </td>
+            <td width='150px'>
+              <BoardRegion>지역</BoardRegion>
+            </td>
+            <td width='400px'>
+              <BoardTitle>기업명/제목</BoardTitle>
+            </td>
+            <td width='100px'>
+              <BoardWriteData>작성날짜</BoardWriteData>
+            </td>
+            <td width='100px'>
+              <BoardClosingDate>마감일</BoardClosingDate>
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default BoardList;
