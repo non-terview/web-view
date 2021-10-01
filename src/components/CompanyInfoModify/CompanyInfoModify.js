30ffd643c43ac351c1567d1ff2b7b7571982268a
ByoungJinYoo <dlwlrma98@kakao.com>
2021-10-01 23:47:53 +0900

    회사정보 수정페이지 #36
    2번째 수정중

--- src/components/CompanyInfoModify/CompanyInfoModify.js
+++ src/components/CompanyInfoModify/CompanyInfoModify.js
@@ -1,17 +1,40 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 export default function CompanyInfoModify() {
   const ContentTr = styled.tr`
-    display: ${(props) => props.display || 'none'};
+    display: ;
+    width: 300px;
+    padding: 20px 40px;
   `;
 
-  function show_content(a) {
-    if (ContentTr.display === '') {
-      ContentTr.display = 'none';
+  const ContentTd = styled.td`
+    background-color: red;
+    padding: 20px 32px;
+    font-size: 13px;
+  `;
+
+  const qnaArr = [
+    {
+      id: 1,
+      title: '기업정보 중 회사명을 변경하고 싶습니다.',
+      contents: '상호명 변경 시~'
+    },
+    { id: 2, title: '기업정보 중 가입자 이름 변경은 어떻게 하나요?', contents: '기업정보 내용이 들어갑니다' }
+  ];
+
+  let url = 'https://contents.albamon.kr/monimg/customer/faq/icon_arrow_open.gif';
+  const [arrow, setArrow] = useState('https://contents.albamon.kr/monimg/customer/faq/icon_arrow_open.gif');
+  const [sub, setSub] = useState('내용열기');
+
+  function show_content() {
+    if (url === arrow) {
+      setArrow('https://contents.albamon.kr/monimg/customer/faq/icon_arrow_close.gif');
+      setSub('내용닫기');
     } else {
-      ContentTr.display = '';
+      setArrow('https://contents.albamon.kr/monimg/customer/faq/icon_arrow_open.gif');
+      setSub('내용열기');
     }
   }
   return (
@@ -19,38 +42,28 @@ export default function CompanyInfoModify() {
       <table>
         <colgroup>
           <col width='30' />
-          <col width='*' />
-          <col width='40' />
+          <col width='400' />
+          <col width='10' />
         </colgroup>
-        <thead></thead>
         <tbody>
-          <tr className='offQ' id='question_0'>
-            <td className='mark'>Q.</td>
-            <td className='question'>
-              <Link to='#' onClick={show_content(0)}>
-                사업자등록번호가 없으면 아이디/비밀번호 찾기를 할 수 없나요?
-              </Link>
-            </td>
-            <td className='arrow'>
-              <Link to='#' onClick={show_content(0)}>
-                <img id='arrow_0' src='https://contents.albamon.kr/monimg/customer/faq/icon_arrow_open.gif' alt='내용열기' />
-              </Link>
-            </td>
-          </tr>
-          <ContentTr>
-            <td colSpan={3}>
-              <div>
-                <b>[기업회원 아이디 찾기 방법]</b>
-                <br />
-                ① 사업자등록번호로 찾기
-                <br />
-                ② 아이핀으로 찾기
-                <br />
-                ③ 이메일로 찾기
-                <br />
-              </div>
-            </td>
-          </ContentTr>
+          {qnaArr.map((data, index) => (
+            <Fragment key={index}>
+              <tr>
+                <td>Q.</td>
+                <td>
+                  <h4>{data.title}</h4>
+                </td>
+                <td>
+                  <Link to='#' onClick={show_content}>
+                    <img src={arrow} alt={sub} />
+                  </Link>
+                </td>
+              </tr>
+              <ContentTr key={data.id}>
+                <ContentTd colSpan='3'>{data.contents}</ContentTd>
+              </ContentTr>
+            </Fragment>
+          ))}
         </tbody>
       </table>
     </Fragment>
