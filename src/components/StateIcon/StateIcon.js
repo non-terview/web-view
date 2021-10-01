fcab26eef2ba6f08ecded0269cb82cd6bd92c66d
Kim Hyunjae <loma1016@gmail.com>
2021-10-01 18:31:39 +0900

    면접 상태 아이콘 #38
    상태값에 따라 다른 색상을 return 해주어 아이콘의 색상을 변경하는 코드를 추가해야함.
    지금 당장 좋은 방법이 생각나지 않아 다른 작업 먼저 할 예정

---
+++ src/components/StateIcon/StateIcon.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+// Styles
+import Colors from '../../styles/Colors';
+import styled from 'styled-components';
+
+const StateIconWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const LeftBoxWrap = styled.div`
+  width: 100px;
+  height: 30px;
+  border-radius: 10px 0 0 10px;
+  background-color: ${Colors.colorDarkGrey};
+  text-align: center;
+`;
+
+const RightBoxWrap = styled.div`
+  width: 40px;
+  height: 30px;
+  border-radius: 0 10px 10px 0;
+  /* props로 background 색상 받아와서 상태에 따라 다른색 표시  */
+  background-color: ${Colors.primaryColor};
+  text-align: center;
+`;
+
+// 후에 리덕스에서 상태값 받아와서 적용하기
+const StateIcon = () => {
+  const [iconState, setIconState] = useState('참여');
+  const stateMap = [
+    {
+      id: 1,
+      state: '참여',
+      color: '#45ff38'
+    },
+    {
+      id: 2,
+      state: '종료',
+      color: '#ff3838'
+    }
+  ];
+  return (
+    <StateIconWrap>
+      <LeftBox />
+      <RightBox />
+    </StateIconWrap>
+  );
+};
+
+const LeftBox = () => {
+  return <LeftBoxWrap>fdf</LeftBoxWrap>;
+};
+const RightBox = () => {
+  return <RightBoxWrap>fdf</RightBoxWrap>;
+};
+
+export default StateIcon;
--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -2,7 +2,8 @@ const Colors = {
   primaryColor: '#267FF7',
   highlightingColor: '#3367FF',
   colorWhite: '#ffffff',
-  colorBlack: '#000000'
+  colorBlack: '#000000',
+  colorDarkGrey: '#424242'
 };
 
 export default Colors;
