2e75a43335cf1843e1406fd09cd9516ef8d3fa44
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-10-06 19:15:02 +0900

    LoginForm Material Ui 작업 #59

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,112 +1,123 @@
-import React, {useState} from 'react';
-
-import styled from 'styled-components';
+import React, { useState } from 'react';
 
 import LoginInput from './LoginInput/LoginInput';
-import Colors from "../../styles/Colors";
+
+import Button from "@material-ui/core/Button";
+
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '600px',
+    },
+    '& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
 
 
-const FormWrap = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
+export default function LoginForm( ) {
+  // 언어 설정
+  const [ lang ] = useState( 'ko' );
+  // 이메일, 비밀번호
+  const [ email, setEmail ] = useState( '' );
+  const [ password, setPassword ] = useState( '' );
+  // 회사 정보
+  const [ company, setCompany ] = useState( '' );
+  // 사용자 / 회사
+  const [ isUser, setIsUser ] = useState( true );
+  const classes = useStyles();
 
-const ToggleButton = styled.input`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 15px;
-  text-transform: uppercase;
-  background-color: ${Colors.primaryColor};
-  color: white;
-  width: 180px;
-  padding: 10px 40px;
-  font-weight: bold;
-  cursor: pointer;
-  margin-top: 20px;
-`;
 
-export default function LoginForm() {
-    // 언어 설정
-    const [lang] = useState('en');
-    // 이메일, 비밀번호
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    // 회사 정보
-    const [company, setCompany] = useState('');
-    // 사용자 / 회사
-    const [isUser, setIsUser] = useState(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const userMap = [
-        {
-            id: 1,
-            placeholder: 'email',
-            setter: setEmail,
-            type: 'text',
-            value: email
-        },
-        {
-            id: 2,
-            placeholder: 'password',
-            setter: setPassword,
-            type: 'password',
-            value: password
-        }
-    ];
+    console.log( email );
+    console.log( password );
+    console.log( company );
+    console.log( isUser );
+  }
 
-    const companyMap = [
-        {
-            id: 1,
-            placeholder: 'email',
-            setter: setEmail,
-            type: 'text',
-            value: email
-        },
-        {
-            id: 2,
-            placeholder: 'password',
-            setter: setPassword,
-            type: 'password',
-            value: password
-        },
-        {
-            id: 3,
-            placeholder: 'company',
-            setter: setCompany,
-            type: 'text',
-            value: company
-        }
-    ];
-
-    function LoginInputView() {
-        return isUser ? userMap : companyMap;
+  const userMap = [
+    {
+      id: 1,
+      placeholder: 'email',
+      setter: setEmail,
+      type: 'email',
+      value: email
+    },
+    {
+      id: 2,
+      placeholder: 'password',
+      setter: setPassword,
+      type: 'password',
+      value: password
     }
+  ];
 
-    const langString = {
-        ko: {
-            email: '이메일',
-            password: '비밀번호',
-            company: '회사'
-        },
-        en: {
-            email: 'email',
-            password: 'password',
-            company: 'company'
-        }
-    };
+  const companyMap = [
+    {
+      id: 1,
+      placeholder: 'email',
+      setter: setEmail,
+      type: 'text',
+      value: email
+    },
+    {
+      id: 2,
+      placeholder: 'password',
+      setter: setPassword,
+      type: 'password',
+      value: password
+    },
+    {
+      id: 3,
+      placeholder: 'company',
+      setter: setCompany,
+      type: 'text',
+      value: company
+    }
+  ];
 
-    return (
-        <FormWrap>
-            <LoginInput lang={lang} langString={langString} loginMap={LoginInputView()}/>
-            <ToggleButton
-                type='button'
-                onClick={() => {
-                    setIsUser(!isUser);
-                }}
-                value={isUser ? '회사 로그인으로 전환' : '개인 사용자 로그인으로 전환'}
-            />
-        </FormWrap>
-    );
+  function LoginInputView() {
+    return isUser ? userMap : companyMap;
+  }
+
+  const langString = {
+    ko: {
+      email: '이메일',
+      password: '비밀번호',
+      company: '회사'
+    },
+    en: {
+      email: 'Email',
+      password: 'Password',
+      company: 'Company'
+    }
+  };
+
+  return (
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <LoginInput lang={ lang } langString={ langString } loginMap={ LoginInputView() }/>
+      <Button
+        variant="outlined"
+        type='button'
+        onClick={ () => {
+          setIsUser( !isUser );
+        } }
+      >{ isUser ? '회사 로그인으로 전환' : '개인 사용자 로그인으로 전환' }</Button>
+      <Button type="submit" variant="contained" color="primary">
+        Signup
+      </Button>
+    </form>
+  );
 }
