d42009c3d05d6d690a4b2c99fcbfb49e7260bb4d
ByoungJinYoo <dlwlrma98@kakao.com>
2021-10-11 21:17:34 +0900

    에러페이지 디자인 #69
    SvgIcon을 이용한 간단한 디자인

--- src/components/ErrorType/Error404.js
+++ src/components/ErrorType/Error404.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import styled from 'styled-components';
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 const ErrorWrap = styled.div`
   height: 650px;
@@ -13,19 +14,24 @@ const ErrorWrap = styled.div`
 const Ptag = styled.div`
   font-size: 20px;
 `;
-
+function SVGErrorIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d='M22,10v8h-2v-8H22z M20,20v2h2v-2H20z M18,17.29C16.53,18.95,14.39,20,12,20c-4.41,0-8-3.59-8-8c0-4.41,3.59-8,8-8v9 l7.55-7.55C17.72,3.34,15.02,2,12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10c2.25,0,4.33-0.74,6-2V17.29z' />
+    </SvgIcon>
+  );
+}
 class Error404 extends Component {
   render() {
     return (
-      <div>
-        <ErrorWrap>
-          <div>
-            <h2>Error (404)</h2>
-            <Ptag>죄송합니다.</Ptag>
-            <Ptag>현재 찾을 수 없는 페이지를 요청 하셨습니다.</Ptag>
-          </div>
-        </ErrorWrap>
-      </div>
+      <ErrorWrap>
+        <SVGErrorIcon style={{ fontSize: '9rem', color: '#3367FF' }} />
+        <div>
+          <h2>Error (404)</h2>
+          <Ptag>죄송합니다.</Ptag>
+          <Ptag>찾을 수 없는 페이지입니다.</Ptag>
+        </div>
+      </ErrorWrap>
     );
   }
 }
--- src/components/ErrorType/Error500.js
+++ src/components/ErrorType/Error500.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import styled from 'styled-components';
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 const ErrorWrap = styled.div`
   height: 650px;
@@ -15,18 +16,24 @@ const Ptag = styled.div`
   font-size: 20px;
 `;
 
+function SVGErrorIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d='M15.73 3H8.27L3 8.27v7.46L8.27 21h7.46L21 15.73V8.27L15.73 3zM12 17.3c-.72 0-1.3-.58-1.3-1.3 0-.72.58-1.3 1.3-1.3.72 0 1.3.58 1.3 1.3 0 .72-.58 1.3-1.3 1.3zm1-4.3h-2V7h2v6z' />
+    </SvgIcon>
+  );
+}
 class Error500 extends Component {
   render() {
     return (
-      <div>
-        <ErrorWrap>
-          <div>
-            <h2>Error (500)</h2>
-            <Ptag>죄송합니다.</Ptag>
-            <Ptag>내부 서버 오류입니다!</Ptag>
-          </div>
-        </ErrorWrap>
-      </div>
+      <ErrorWrap>
+        <SVGErrorIcon style={{ fontSize: '9rem', color: '#3367FF' }} />
+        <div>
+          <h2>Error (500)</h2>
+          <Ptag>죄송합니다.</Ptag>
+          <Ptag>내부 서버 오류입니다!</Ptag>
+        </div>
+      </ErrorWrap>
     );
   }
 }
