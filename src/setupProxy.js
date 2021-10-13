48731202d9754922279531db4adfd702df76e12d
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-10-13 16:33:00 +0900

    login 및 사용자 데이터 가져오기 #72

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,12 @@
 const { createProxyMiddleware } = require( 'http-proxy-middleware' );
 
-module.exports = function ( app ) {
+module.exports = function ( app ){
   app.use(
-    '/api',
-    createProxyMiddleware( {
-      target: "https://www.askephoenix.site/api",
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': '' // 하위 url 초기화
-      }
-    } )
-  );
-};
+    "/api",
+    createProxyMiddleware({
+      target: "https://www.askephoenix.site",
+      changeOrigin: true
+    })
+  )
+}
+
