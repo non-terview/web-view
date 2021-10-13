a11ec7cfb19c83bdd3281c1b059851e87ae1a7e2
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-10-13 18:09:48 +0900

    build 오류 수정 #76
    - 불필요 import 제거

--- src/components/Template/Header/Header.js
+++ src/components/Template/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector} from 'react-redux';
 
 import { selectUserInfo } from '../../../redux/User/LoginUserSlice'
 
