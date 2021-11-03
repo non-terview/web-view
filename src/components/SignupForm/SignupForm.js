005bf8b687acd4d3e6a9347e3494797dbfe040ff
Kim Hyunjae <loma1016@gmail.com>
2021-11-03 14:43:41 +0900

    post 수정 #88

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -11,26 +11,6 @@ import SignupInput from './SignupInput';
 // hooks
 import useInput from '../../hooks/useInput';
 
-import Colors from '../../styles/Colors';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: theme.spacing(2),
-
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '600px'
-    },
-    '& .MuiButtonBase-root': {
-      margin: theme.spacing(2)
-    }
-  }
-}));
-
 const FormWrap = styled.form`
   display: flex;
   align-items: center;
@@ -40,7 +20,7 @@ const FormWrap = styled.form`
 const ButtonContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   margin-top: 20px;
 `;
 
@@ -52,7 +32,31 @@ const SignupForm = (props) => {
   // 일반유저, 회사 설정 -> 후에 버튼도 개별적인 컴포넌트로 뺄 예정
   const [isUser, setIsUser] = useState(true);
 
-  const signUp = async () => {};
+  const signUp = async () => {
+    if (password !== confirmPassword) {
+      console.error('not confirm');
+      return;
+    }
+    // 일치했을 경우 POST ->
+    const { data: token } = await axios.get('/api/user/token');
+    await axios
+      .post('/api/user', {
+        username: email,
+        password: password,
+        name: name,
+        auth: 'ROLE_USER',
+        type: 'USER',
+        _csrf: token.token.token
+        //
+      })
+      .then((r) => {
+        console.log(r);
+      })
+      .catch((res) => {
+        console.error('err');
+      });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     signUp().then((r) => {
--- src/components/SignupForm/SignupInput.js
+++ src/components/SignupForm/SignupInput.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Colors from '../../styles/Colors';
 import styled from 'styled-components';
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 const SignInput = styled.input`
   width: 180px;
   padding: 7px 10px;
@@ -15,17 +18,42 @@ const SignInput = styled.input`
   }
 `;
 
+const InputWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 600px;
+`;
+
+const InputField = styled(TextField)`
+  margin-top: 5px;
+  width: 100;
+`;
+
 const SignupInput = (props) => {
   const mapData = props.mapData.map((index) => (
-    <div key={index.id}>
-      <SignInput
+    <InputWrap key={index.id}>
+      {/* <SignInput
+        key={index.id}
+        placeholder={index.placeholder}
+        name={index.name}
+        value={index.value}
+        onChange={index.setter}
+      /> */}
+      <InputField
+        fullWidth
+        type={index.type}
+        label={index.name}
+        variant='standard'
+        required
         key={index.id}
         placeholder={index.placeholder}
         name={index.name}
         value={index.value}
         onChange={index.setter}
       />
-    </div>
+    </InputWrap>
   ));
 
   return <div>{mapData}</div>;
