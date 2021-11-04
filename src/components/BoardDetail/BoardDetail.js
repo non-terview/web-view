44483d5c0d17131c7d667fb77a612d87759e0029
ByoungJinYoo <dlwlrma98@kakao.com>
2021-11-04 22:16:35 +0900

    게시판 상세화면 # 90
    게시판 상세화면 레이아웃

--- src/components/BoardDetail/BoardDetail.js
+++ src/components/BoardDetail/BoardDetail.js
@@ -1,44 +1,7 @@
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Fragment, React } from 'react';
-
-const propTypes = {};
-
-const defaultProps = {};
-//게시판 내용이 들어가는 부분
-const ContentHeader = styled.div`
-  width: 800px;
-  height: 300px;
-  background-color: grey;
-  display: flex;
-  margin: 50px auto;
-`;
-
-const ContentContainer = styled.section`
-  position: relative;
-  width: 960px;
-  height: 1500px;
-  margin: 0 auto;
-  background-color: skyblue;
-`;
-const ContentFooter = styled.div`
-  width: 800px;
-  height: 700px;
-  background-color: grey;
-  display: flex;
-  margin: 100px auto;
-`;
+import { React } from 'react';
+import DetailContent from '../BoardDetail/DetailContent';
 
 export default function BoardDetailScreen(props) {
-  return (
-    <Fragment>
-      <ContentContainer>
-        <ContentHeader>아아아</ContentHeader>
-        <ContentFooter>상세정보 및 조건이 보여지는곳이에요</ContentFooter>
-      </ContentContainer>
-    </Fragment>
-  );
+  return <DetailContent />;
 }
-
-BoardDetailScreen.propTypes = propTypes;
-BoardDetailScreen.defaultProps = defaultProps;
