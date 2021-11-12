d2eebf718db0b6f1f672d46061233562a01bc037
Kim Hyunjae <loma1016@gmail.com>
2021-11-12 15:15:52 +0900

    레이아웃 파일 분할 작업 #89

--- src/components/Interview/Interview.js
+++ src/components/Interview/Interview.js
@@ -6,6 +6,7 @@ import ColorPalette from './InterviewColor';
 // components
 import Sidebar from './Sidebar';
 import MenuItem from './MenuItem';
+import Video from './Video';
 
 const Container = styled.div`
   display: flex;
@@ -26,43 +27,6 @@ const ContentContainer = styled.div`
   background-color: orange;
 `;
 
-// video
-const VideoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  width: 80%;
-  height: 100%;
-  background-color: ${ColorPalette.VideoBackground};
-`;
-const VideoMain = styled.div`
-  width: 640px;
-  height: 360px;
-  background-color: ${ColorPalette.VideoTestColor};
-`;
-const VideoSubContainter = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  padding: 10px;
-  width: 100%;
-  height: 20%;
-  margin-top: 10%;
-`;
-const VideoSub = styled.div`
-  width: calc(16 * 10px);
-  height: calc(9 * 10px);
-  background-color: ${ColorPalette.VideoTestColor};
-  transition: 0.2s ease-in-out;
-  cursor: pointer;
-  &:hover {
-    width: calc(16 * 12px);
-    height: calc(9 * 12px);
-  }
-`;
-
 const Interview = () => {
   return (
     <Container>
@@ -76,25 +40,9 @@ const Interview = () => {
 const Content = () => {
   return (
     <ContentContainer>
-      <VideoView />
+      <Video />
       <MenuItem />
     </ContentContainer>
-  );
-};
-
-// VideoSub 클릭시 VideoMain과 스왑
-// VideoMain컴포넌트와 VideoSub컴포넌트 분할
-const VideoView = () => {
-  return (
-    <VideoContainer>
-      <VideoMain />
-      <VideoSubContainter>
-        <VideoSub />
-        <VideoSub />
-        <VideoSub />
-        <VideoSub />
-      </VideoSubContainter>
-    </VideoContainer>
   );
 };
 
